526b0907f4edb7a1972c2844df913f922dbd90ea
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2025-03-04 17:04:07 +0600

    fixed camera ui

--- lib/home/camera_screen.dart
+++ lib/home/camera_screen.dart
@@ -243,20 +243,22 @@ class CameraScreenState extends State<CameraScreen> {
 class ScanFramePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final paint =
+    final Paint paint =
         Paint()
-          ..color = const Color.fromARGB(255, 255, 255, 255)
-          ..strokeWidth = 6
+          ..color = Colors.white
+          ..strokeWidth = 7
+          ..strokeCap = StrokeCap.round
           ..style = PaintingStyle.stroke;
 
-    final frameWidth = size.width * 0.8;
-    final frameHeight = frameWidth * 0.8;
-    final left = (size.width - frameWidth) / 2;
-    final top = (size.height - frameHeight) * 0.44;
-    final cornerLength = 45.0;
+    final double frameWidth = size.width * 0.8;
+    final double frameHeight = frameWidth * 0.8;
+    final double left = (size.width - frameWidth) / 2;
+    final double top = (size.height - frameHeight) * 0.40;
+    final double cornerLength = 45.0;
 
     canvas.drawLine(Offset(left, top + cornerLength), Offset(left, top), paint);
     canvas.drawLine(Offset(left, top), Offset(left + cornerLength, top), paint);
+
     canvas.drawLine(
       Offset(left + frameWidth - cornerLength, top),
       Offset(left + frameWidth, top),
@@ -265,6 +267,28 @@ class ScanFramePainter extends CustomPainter {
     canvas.drawLine(
       Offset(left + frameWidth, top),
       Offset(left + frameWidth, top + cornerLength),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(left, top + frameHeight - cornerLength),
+      Offset(left, top + frameHeight),
+      paint,
+    );
+    canvas.drawLine(
+      Offset(left, top + frameHeight),
+      Offset(left + cornerLength, top + frameHeight),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(left + frameWidth - cornerLength, top + frameHeight),
+      Offset(left + frameWidth, top + frameHeight),
+      paint,
+    );
+    canvas.drawLine(
+      Offset(left + frameWidth, top + frameHeight),
+      Offset(left + frameWidth, top + frameHeight - cornerLength),
       paint,
     );
   }
